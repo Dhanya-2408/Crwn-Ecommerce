77a524e02475e60792dd76c3f317af44a6c9db82
Dhanya <dhanyaanu888@gmail.com>
2022-03-26 18:47:27 +0530

    Styled component Implementation For routes

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Category from '../category/category.component';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
-import './shop.styles.scss';
+
 
 const Shop = () => {
   return (
