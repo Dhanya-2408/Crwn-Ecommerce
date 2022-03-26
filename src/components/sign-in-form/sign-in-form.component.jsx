e6ab45d2055710c844891ffd8a84e6c25154ebe2
Dhanya <dhanyaanu888@gmail.com>
2022-03-26 19:11:07 +0530

    Styled Component Implementation

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,13 +1,11 @@
-import { useState} from 'react';
+import { useState } from 'react';
 import FormInput from '../form-input/form-input.component';
-import Button from '../button/button.component';
-
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 import {
-  signInWithGooglePopup,
   signInAuthUserWithEmailAndPassword,
+  signInWithGooglePopup,
 } from '../../utils/firebase/firebase.utils';
-
-import './sign-in-form.styles.scss';
+import { SignInContainer, ButtonsContainer } from './sign-in-form.styles';
 
 const defaultFormFields = {
   email: '',
@@ -70,7 +68,7 @@ const SignInForm = () => {
   };
 
   return (
-    <div className='sign-up-container'>
+    <SignInContainer>
       <h2>Already have an account?</h2>
       <span>Sign in with your email and password</span>
       <form onSubmit={handleSubmit}>
@@ -93,7 +91,7 @@ const SignInForm = () => {
         />
         <div className='buttons-container'>
           <Button type='submit'>Sign In</Button>
-          <Button type='button' buttonType='google' onClick={signInWithGoogle}>
+          <Button type='button' buttonType={BUTTON_TYPE_CLASSES.google} onClick={signInWithGoogle}>
             Google sign in
           </Button>
            {/* <Button type='button' buttonType='google' onClick={signInWithGoogle}>
@@ -101,7 +99,7 @@ const SignInForm = () => {
           </Button> */}
         </div>
       </form>
-    </div>
+    </SignInContainer>
   );
 };
 
